6866be663355b76bcff5f8cb71e861cedc748b13
MichaelCheung105 <35844248+MichaelCheung105@users.noreply.github.com>
2018-06-27 20:16:14 +0800

    1st python file for building agent

---
+++ building_agent.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 27 20:11:01 2018
+
+@author: lupus
+"""
+
+max_episode = 10
+episode = 1
+
+while episode < max_episode:
+    matrix = [0]*9
+    
+    while 0 in matrix:
+        #black move
+        
+        
+        #white move
+        
+        break
+    #if black wins, reward +1
+    #if draw wins, reward +0
+    #if black lose, reward -1
+    
+    episode +=1
