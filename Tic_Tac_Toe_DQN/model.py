50fd02446673c49e172aef59c29c1cd0e99d2650
Michael Cheung <lupus.cheung@gmail.com>
2019-04-20 22:19:54 +0800

    add logging

--- Tic_Tac_Toe_DQN/model.py
+++ Tic_Tac_Toe_DQN/model.py
@@ -6,13 +6,9 @@ from keras.layers.normalization import BatchNormalization
 class DQN:
     def __init__(self, shape):
         self.model = Sequential()
-        self.model.add(Conv2D(32, kernel_size=3, padding='same', activation='relu', input_shape=shape))
-        self.model.add(Dropout(0.2))
-        self.model.add(Conv2D(64, kernel_size=3, padding='same', activation='relu'))
-        self.model.add(Dropout(0.2))
+        self.model.add(Conv2D(32, kernel_size=3, activation='relu', input_shape=shape))
         self.model.add(Flatten())
         self.model.add(Dense(32, activation='relu'))
-        self.model.add(BatchNormalization())
         self.model.add(Dense(9, activation='linear'))
         self.model.compile(optimizer='adam', loss="mae")
         self.model.summary()
--- Tic_Tac_Toe_DQN/runner.py
+++ Tic_Tac_Toe_DQN/runner.py
@@ -1,4 +1,6 @@
 # Import Packages and Modules
+import pandas as pd
+import matplotlib.pyplot as plt
 from environment import Environment
 from agent import Agent
 from configuration import Config
@@ -11,24 +13,38 @@ class Runner:
         self.player = {0: first_mover,
                        1: second_mover
                        }
+        self.trained_episode = 0
+        self.logs = {"both_greedy": {},
+                    "first_player_random": {},
+                    "second_player_random": {}
+                     }
 
     def start(self):
         for train_episode in range(self.config.total_train_episode):
             if train_episode % self.config.intermediate_test_frequency == 0:
                 self.config.run_mode = 'test'
                 for test_episode in range(self.config.number_of_episode_per_intermediate_test):
-                    self.play_tic_tac_toe(test_episode)
+                    self.play_tic_tac_toe(test_episode, "both_greedy")
+                    self.play_tic_tac_toe(test_episode, "first_player_random")
+                    self.play_tic_tac_toe(test_episode, "second_player_random")
             else:
                 self.config.run_mode = 'train'
                 self.play_tic_tac_toe(train_episode)
+                self.trained_episode = train_episode
 
-    def play_tic_tac_toe(self, episode):
-        epsilon = max(1 - episode/self.config.total_train_episode, self.config.min_epsilon) if self.config.run_mode == 'train' else 0
+        self.log_performance()
+
+        for key, player in self.player.items():
+            player.eval_net.model.save(f"./models/player_{key}.h5")
+
+    def play_tic_tac_toe(self, episode, test_condition=None):
         state, done, info = self.env.reset()
-        player = 0
+        player = 1
         print(f"{self.config.run_mode} mode, episode {episode}, {info}")
 
         while not done:
+            player = 1 - player
+            epsilon = self.determine_epsilon(episode, player, test_condition)
             action = self.player[player].get_action(state, epsilon)
             self.player[player].store_s_a(state, action)
             state, reward, done, info = self.env.step(action, player)
@@ -37,13 +53,38 @@ class Runner:
             if done:
                 self.player[player].store_r_s_d(reward, state, done)
 
-            player = 1 - player
             print(f"{self.config.run_mode} mode, episode {episode}: {info}")
 
-        for key in self.player.keys():
-            self.player[key].reset()
-            self.player[key].train()
+        for key, player in self.player.items():
+            player.reset()
+            player.train()
 
+        if self.config.run_mode == 'test':
+            self.record_result_of_first_player(player, reward, test_condition)
+
+    def determine_epsilon(self, episode, player, test_condition):
+        if player == 0 and test_condition == "first_player_random":
+            epsilon = 1
+        elif player == 1 and test_condition == "second_player_random":
+            epsilon = 1
+        else:
+            epsilon = self.default_epsilon(episode)
+        return epsilon
+
+    def default_epsilon(self, episode):
+        epsilon = max(1 - episode / self.config.total_train_episode, self.config.min_epsilon) if self.config.run_mode == 'train' else 0
+        return epsilon
+
+    def record_result_of_first_player(self, player, reward, test_condition):
+        reward = reward if player == 0 else -reward
+        self.logs[test_condition][self.trained_episode] += reward
+
+    def log_performance(self):
+        for test_condition, records in self.logs.items():
+            df = pd.DataFrame.from_dict(records)
+            fig = df.plot(title=test_condition)
+            fig.savefig(f"./performance/{test_condition}.jpg")
+            plt.close()
 
 if __name__ == "__main__":
     env = Environment()
